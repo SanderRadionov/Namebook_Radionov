675e1db766735e5f23b0efdc3ae0d06a2ea59a3a
Opilane TTHK <opilane@tthk.ee>
2018-10-04 15:48:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/opilane/namebook/ShowActivity.java
@@ -0,0 +1,14 @@
+package com.example.opilane.namebook;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ShowActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+    }
+
+}
